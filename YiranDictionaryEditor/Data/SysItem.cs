ad4128ac6082ba2911ae5ad65f33958c1347f130
yusais_life@qq.com <39078847+helloyuzz@users.noreply.github.com>
2020-09-04 18:33:43 +0800

    Create SysItem.cs

---
+++ YiranDictionaryEditor/Data/SysItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YiranDictionaryEditor.Data {
+    public class SysItem {
+        public string name {
+            get;set;
+        }
+        public DateTime create_date {
+            get;set;
+        }
+        public DateTime modify_date {
+            get;set;
+        }
+    }
+}
